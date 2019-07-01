3d492ebb48b239fb60df24cbada67797c4c254d2
qnaiv <qnaiv@gmail.com>
2019-07-02 00:31:41 +0900

    update pom.xml:  dependenciesにaxonを追加

---
+++ src/main/java/pw/qnv/axonhelloworld/AxonHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package pw.qnv.axonhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxonHelloWorldApplication.class, args);
+	}
+
+}
